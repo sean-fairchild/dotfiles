4a0c27c0f34ef9d2e66eec0827467bd147a5db9a
Sean Fairchild <s3ff@pge.com>
2024-07-18 19:10:17 -0400

    exit omz install

--- .chezmoiscripts/run_once_before_install-omz.sh
+++ .chezmoiscripts/run_once_before_install-omz.sh
@@ -2,6 +2,7 @@
 
 if [ ! -e "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  exit 0
 else
   echo '\~/.oh-my-zsh directory exists'
 fi
