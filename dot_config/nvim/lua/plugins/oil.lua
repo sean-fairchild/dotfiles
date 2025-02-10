0c88cf1c217584398493a3d0480e304b981eefdb
Sean Fairchild <s3ff@pge.com>
2025-02-10 14:18:31 -0500

    Add .config/nvim/lua/plugins/nvim-cmp.lua Update .config/nvim/lua/plugins/oil.lua

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -6,4 +6,13 @@ return {
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  keys = {
+    {
+      "<leader>o",
+      function()
+        require("oil").toggle_float()
+      end,
+      desc = "Toggle Oil buffer",
+    },
+  },
 }
