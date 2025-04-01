a94d800f89efb3a0c262607b40ee8fcb7fd1e546
Sean Fairchild <s3ff@pge.com>
2025-04-01 13:18:13 -0400

    Update .config/nvim/lua/plugins/carbon.lua Update .config/nvim/lua/plugins/copilot-chat.lua Add .config/nvim/lua/plugins/gruvbox.lua

--- dot_config/nvim/lua/plugins/carbon.lua
+++ dot_config/nvim/lua/plugins/carbon.lua
@@ -1,21 +1,22 @@
-return {
-  {
-    "SidOfc/carbon.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    lazy = false,
-    keys = {
-      {
-        "<leader>e",
-        function()
-          require("carbon").toggle_sidebar({ bang = true })
-        end,
-        desc = "Toggle Carbon",
-      },
-    },
-    opts = {},
-    config = function()
-      require("carbon").setup({})
-    end,
-  },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-}
+return {}
+-- return {
+--   {
+--     "SidOfc/carbon.nvim",
+--     dependencies = { "nvim-tree/nvim-web-devicons" },
+--     lazy = false,
+--     keys = {
+--       {
+--         "<leader>e",
+--         function()
+--           require("carbon").toggle_sidebar({ bang = true })
+--         end,
+--         desc = "Toggle Carbon",
+--       },
+--     },
+--     opts = {},
+--     config = function()
+--       require("carbon").setup({})
+--     end,
+--   },
+--   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+-- }
--- dot_config/nvim/lua/plugins/copilot-chat.lua
+++ dot_config/nvim/lua/plugins/copilot-chat.lua
@@ -1,3 +1,4 @@
+return {}
 -- return {
 --   "CopilotC-Nvim/CopilotChat.nvim",
 --   lazy = false,
